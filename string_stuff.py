c5879181aeeccfb93a5614b12817eb82f1c7de92
Rome314 <401206176@mail.ru>
2018-09-13 02:53:57 +0300

    ___

---
+++ string_stuff.py
@@ -0,0 +1,13 @@
+E1 = "E1: A state s is not in set of states"
+E2 = "E2: Some states are disjoint"
+E3 = "E3: A transition a is not represented in the alphabet"
+E4 = "E4: Initial state is not defined"
+E5 = "E5: Input file is malformed"
+
+R1 = "FSA is complete"
+R2 = "FSA is incomplete"
+
+W1 = "W1: Accepting state is not defined"
+W2 = "W2: Some states are not reachable from initial state"
+W3 = "W3: FSA is nondeterministic"
+
